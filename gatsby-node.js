6eeabfed7a17b4a89105460187861defe05dba7d
DavidNora <david_nora_ext@carrefour.com>
2023-02-28 13:31:44 -0300

    teste redirect 1

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,11 +8,31 @@
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+  const { createPage ,createRedirect} = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+
+  createRedirect({
+    fromPath: "/using-typescript",
+    toPath: `/teste2`,
+    statusCode: 200,
+    proxyHeaders: {
+      "x-forwarded-host": `https://mercado.carrefour.com.br/`,
+      via: "''",
+    },
+  })
+
+  createRedirect({
+    fromPath: "/sitemap",
+    toPath: `https://carrefourbrfood.myvtex.com/sitemap.xml`,
+    statusCode: 200,
+    proxyHeaders: {
+      "x-forwarded-host": `https://mercado.carrefour.com.br/`,
+      via: "''",
+    },
   })
 }
