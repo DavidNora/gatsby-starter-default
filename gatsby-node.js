5aa1b66061e5857cad81af9aff2409626eb0868e
DavidNora <david_nora_ext@carrefour.com>
2023-02-28 13:51:46 -0300

    teste 2 redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ actions }) => {
-  const { createPage ,createRedirect} = actions
+  const { createPage, createRedirect } = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
@@ -33,6 +33,44 @@ exports.createPages = async ({ actions }) => {
     proxyHeaders: {
       "x-forwarded-host": `https://mercado.carrefour.com.br/`,
       via: "''",
+    },
+  })
+
+  createRedirect({
+    fromPath: `/sitemap2.xml`,
+    toPath: `https://mercado.carrefour.vtexcommercestable.com.br/sitemap.xml`,
+    conditions: {
+      "x-forwarded-host":
+        "https://mercado.carrefour.com.br (https://mercado.carrefour.com.br/)",
+    },
+  })
+
+  createRedirect({
+    fromPath: `/sitemap3.xml`,
+    toPath: `https://carrefourbrfood.myvtex.com/sitemap.xml`,
+    conditions: {
+      "x-forwarded-host":
+        "https://mercado.carrefour.com.br (https://mercado.carrefour.com.br/)",
+    },
+  })
+
+  createRedirect({
+    fromPath: `/sitemap4.xml`,
+    toPath: `https://carrefourbrfood.myvtex.com/sitemap.xml`,
+    statusCode: 200,
+    conditions: {
+      "x-forwarded-host":
+        "https://mercado.carrefour.com.br (https://mercado.carrefour.com.br/)",
+    },
+  })
+
+  createRedirect({
+    fromPath: `/sitemap5.xml`,
+    toPath: `https://mercado.carrefour.vtexcommercestable.com.br/sitemap.xml`,
+    statusCode: 200,
+    conditions: {
+      "x-forwarded-host":
+        "https://mercado.carrefour.com.br (https://mercado.carrefour.com.br/)",
     },
   })
 }
